e385f251b895205daaed7c24a5e84cf3fe1863fe
Sardar Hassan <65546777+sardarhassan10@users.noreply.github.com>
2021-01-24 13:54:10 +0500

    Update Node_to_RPI.ino

--- Node_to_RPI.ino
+++ Node_to_RPI.ino
@@ -1,106 +1,120 @@
-//libraries
-#include <ESP8266WiFi.h>        //https://github.com/esp8266/Arduino
+#include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>    //https://github.com/tzapu/WiFiManager
 #include <PubSubClient.h>
-//change these according to your pin numbers 
-#define LO_plus 10
-#define LO_minus 11
-#define input A0
-#define time_period 5
 
+#define LOM_PIN 4 // LO+ pin
+#define LOP_PIN 5 // LO- pin
+#define SAMPLING_TIME 60 // In seconds
+
+// WiFi access point credentials
+
+const char * ssid = "WiFi Name";
+const char * password = "WiFi Password";
+
+// MQTT server IP address and port
+
+const char * mqttServer = "192.168.100.137";
+const int mqttPort = 1883;
+
+// Global variables
+
+bool stop = false; // Switch to prevent further sampling
+String to_send; // Data to send
 int time_start;
-const char* mqttServer = "192.168.0.110";  //change server
-const int mqttPort = 1883;      // change port
+int count = 0;
 
-void callback(char* topic, byte* payload, unsigned int length) {
- 
+// callback(): Print any messages received from the broker (unused function)
+
+void callback(char * topic, byte * payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
+
   Serial.print("Message:");
   Serial.println();
-  
+
   String message = "";
   for (int i = 0; i < length; i++) {
-   Serial.print((char)payload[i]);
-    message+=(char)payload[i];
+    Serial.print((char) payload[i]);
+    message += (char) payload[i];
   }
+
   Serial.println("-----------------------");
-//  if(String(topic)=="LED"){
-//    if(message=="LED ON"){
-//      //digitalWrite(LED,HIGH);
-//      Serial.println("Message received");
-//    }
-//    else{
-//      //digitalWrite(LED,LOW);
-//      Serial.println("Message not received");
-//    }
-//  }
-     
 }
+
+// Create WiFi and MQTT clients
 
 WiFiClient espClient;
 PubSubClient client(mqttServer, mqttPort, callback, espClient);
 
 void setup() {
-  // initialize the serial communication:
   Serial.begin(9600);
-     
-  WiFiManager wifiManager;
-  //first parameter is name of access point, second is the password
-  wifiManager.autoConnect("AP-NAME", "AP-PASSWORD");
-  Serial.println("connected");
 
-   client.setServer(mqttServer, mqttPort);
-   client.setCallback(callback);
-    while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
- 
-    if (client.connect("ESP8266Client" )) {
-      client.subscribe("LED");
-      Serial.println("connected");
- 
+  pinMode(LOM_PIN, INPUT); // Setup for leads off detection LO-
+  pinMode(LOP_PIN, INPUT); // Setup for leads off detection LO+
+
+  // Connect to WiFi
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    Serial.println("Connecting to RPi MQTT server...");
+
+    if (client.connect("ESP8266Client")) {
+      client.subscribe("ECG");
+      Serial.println("Connected!");
     } else {
- 
-      Serial.print("failed with state ");
+      Serial.print("Failed with state ");
       Serial.print(client.state());
       delay(2000);
- 
     }
-}
-  // change pin numbers
-  pinMode(LO_plus, INPUT); // Setup for leads off detection LO +
-  pinMode(LO_minus, INPUT); // Setup for leads off detection LO -
-  time_start=millis();
+  }
+
+  time_start = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!stop) {
+    if ((digitalRead(LOM_PIN) == 1) || (digitalRead(LOP_PIN) == 1)) { // If leads are not connected...
+      if (count < 10) count++;
+      else count = 0;
+      to_send = String(count); // Data to send (for debugging purposes)
+    } else {
+      // Read ECG signal
+      Serial.println(analogRead(A0));
+      to_send = String(analogRead(A0));
+    }
 
-  //Wait for a bit to keep serial data from saturating
-  delay(2);
-  int time_now= millis();
-  int time_to_send = time_now-time_start;
-  if (time_to_send >= time_period){
-    
-    if((digitalRead(LO_plus) == 1)||(digitalRead(LO_minus) == 1)){
-    Serial.println('!');
-    String toSend = String('!');
-    client.publish("data",toSend.c_str());
+    client.publish("ECG/data", to_send.c_str()); // Publish to MQTT server on topic "ECG/data"
+
+    // Wait for a bit to keep serial data from saturating
+
+    delay(2);
+
+    // Send timestamp
+
+    int time_now = millis();
+    to_send = String(time_now - time_start);
+    client.publish("ECG/data", to_send.c_str());
+
+    // Terminate after sampling time is over
+
+    if (time_now - time_start > SAMPLING_TIME * 1000) {
+      stop = true;
+      Serial.println("Session terminated.");
+    }
   }
-  else{
-    // send the value of analog input 0:
-      Serial.println(analogRead(input)); /// change value of port
-      String toSend = String(analogRead(input));
-      client.publish("data",toSend.c_str());
-  }  
-  Serial.println(time_to_send);
-  String toSend_1 = String(time_to_send);
-  client.publish("data",toSend_1.c_str());
-  time_start=millis();
-  }
-
-
 }
